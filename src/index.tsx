3d7d7d7be00a6a14186386a856c63e9d8b4f594a
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-10-03 18:40:26 +1300

    Firebase installation and setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAObGMTlQhtDzdDiO8ygNMDd7rXYkuaXI0",
+  authDomain: "test-project-96455.firebaseapp.com",
+  projectId: "test-project-96455",
+  storageBucket: "test-project-96455.appspot.com",
+  messagingSenderId: "325982930563",
+  appId: "1:325982930563:web:f6b9c1c35c33baf5d3bdb6",
+  measurementId: "G-LTH4VFETKQ",
+};
+
+const app = initializeApp(firebaseConfig);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export const auth = getAuth(app);
