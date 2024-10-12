fa9d92c93c908f64c2e588762c8864174181f6dd
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-10-13 12:48:38 +1300

    install react-toastify

--- src/index.tsx
+++ src/index.tsx
@@ -2,35 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-};
-
-const app = initializeApp(firebaseConfig);
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <ToastContainer />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
-export const auth = getAuth(app);
