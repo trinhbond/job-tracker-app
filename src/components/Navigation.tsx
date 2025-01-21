b3eccac6cc00b703cea27295ded47f34229a927b
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2025-01-21 13:34:09 +1300

    Styling dropdown

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,6 @@
 import { useContext, useRef } from "react";
 import { AuthContext } from "../context/AuthContext";
 import Link from "./Link";
-import AccountsCircle from "./icons/AccountsCircle";
-import SignOut from "./icons/SignOut";
 import { useMouse } from "../utils";
 
 export default function Navigation() {
@@ -12,38 +10,40 @@ export default function Navigation() {
   const { clicked, setClicked } = useMouse(ref);
 
   return (
-    <div className="sticky w-full bg-black text-white z-10 py-6 px-8 shadow-lg flex flex-row items-center justify-end gap-4">
+    <div className="sticky bg-black text-white z-10 py-6 px-8 shadow-lg flex flex-row items-center justify-end gap-4">
       <Link route="content">Applications</Link>
-      <div className="h-8 bg-red-200 border-l border-red-200"> </div>
+      <div className="h-8 border-l border-white"> </div>
       {user && (
-        <div className="relative" ref={ref}>
+        <div ref={ref}>
           <button onClick={() => setClicked((clicked) => !clicked)}>
-            <span className="bg-red-800 text-white rounded-full text-center block leading-8 min-w-8 min-h-8 font-medium px-2">
+            <span className="bg-red-800 rounded-full text-center block leading-8 min-w-8 min-h-8 font-medium px-2">
               {firstLetter}
             </span>
           </button>
           {!!clicked && (
-            <div className="modal z-[9999] shadow-lg rounded-md min-w-52 w-auto fixed bg-[#333333] text-[13px] text-white right-8 top-16 [&_a]:p-3">
-              <div className="p-3 border-b border-white">
+            <div className="z-[9999] px-3 pt-4 pb-3 shadow-lg rounded-md min-w-52 w-auto fixed bg-[#18181b] text-[13px] right-8 top-16">
+              <div className="border-b border-[#c6c6c6] pb-3 px-2">
                 <h2 className="font-semibold">{user.displayName}</h2>
                 <span className="block">{user.email}</span>
               </div>
-              <div>
-                <Link
-                  route="profile"
-                  className="flex flex-row items-center gap-2 cursor-pointer hover:bg-[#7a7a7a]"
-                >
-                  <AccountsCircle />
-                  <span>Profile</span>
-                </Link>
-                <div
-                  className="flex flex-row items-center gap-2 cursor-pointer p-3 border-t border-white hover:bg-[#7a7a7a]"
-                  onClick={signOutUser}
-                >
-                  <SignOut />
-                  <span>Sign out</span>
-                </div>
-              </div>
+              <ul className="pt-2">
+                <li>
+                  <Link
+                    route="profile"
+                    className="inline-block w-full py-1 px-2 cursor-pointer hover:bg-[#2b2b2b] hover:rounded-md"
+                  >
+                    <span>Account</span>
+                  </Link>
+                </li>
+                <li>
+                  <div
+                    className="py-1 px-2 cursor-pointer hover:bg-[#2b2b2b] hover:rounded-md"
+                    onClick={signOutUser}
+                  >
+                    <span>Sign out</span>
+                  </div>
+                </li>
+              </ul>
             </div>
           )}
         </div>
