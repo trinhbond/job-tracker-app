2059af62fbecc0179f03f75c6d05f6d0362039ee
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2025-03-16 22:11:21 +1300

    UI tweak

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -73,181 +73,173 @@ export default function Home() {
 
   return (
     <div className="h-dvh flex flex-row justify-between [&>div]:w-full">
-      <div>
-        <div className="border border-[#c6c6c6] h-dvh place-content-center flex flex-col gap-6 p-6 bg-white shadow-lg">
-          <h1 className="text-2xl py-3 font-semibold block lg:hidden md:hidden sm:block xs:block">
-            Welcome to jobtracker
-          </h1>
-          {!userHasAccount ? (
-            <form
-              className="flex flex-col gap-4 text-sm"
-              onSubmit={handleSignup}
-            >
-              <div className="flex flex-col gap-1">
-                <label>Name</label>
-                <input
-                  type="text"
-                  className={clsx(
-                    errors.name && "border-red-600",
-                    "border border-[#c6c6c6] rounded-md px-4 py-2 focus:outline-none"
-                  )}
-                  placeholder="Name"
-                  {...register("name", {
-                    pattern: {
-                      value: /^[A-Za-z]+$/i,
-                      message: "Name cannot have symbols or special characters",
-                    },
-                    required: {
-                      value: true,
-                      message: "Name is required",
-                    },
-                  })}
-                />
-                {errors.name && (
-                  <p className="text-red-600 top-0 relative" role="alert">
-                    {errors.name.message}
-                  </p>
-                )}
-              </div>
-              <div className="flex flex-col gap-1">
-                <label>Email</label>
-                <input
-                  type="text"
-                  className={clsx(
-                    errors.email && "border-red-600",
-                    "border border-[#c6c6c6] rounded-md px-4 py-2 focus:outline-none"
-                  )}
-                  placeholder="Email"
-                  {...register("email", {
-                    required: {
-                      value: true,
-                      message: "Email is required",
-                    },
-                    pattern: {
-                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                      message: "Email is invalid",
-                    },
-                  })}
-                />
-                {errors.email && (
-                  <p className="text-red-600" role="alert">
-                    {errors.email.message}
-                  </p>
-                )}
-              </div>
-              <div className="flex flex-col gap-1">
-                <label>Password</label>
-                <input
-                  type="password"
-                  className={clsx(
-                    errors.password && "border-red-600",
-                    "border border-[#c6c6c6] rounded-md px-4 py-2 focus:outline-none"
-                  )}
-                  placeholder="Password (6 or more characters)"
-                  {...register("password", {
-                    required: {
-                      value: true,
-                      message: "Password is required",
-                    },
-                    minLength: {
-                      value: 6,
-                      message: "Password must be 6 characters or more",
-                    },
-                  })}
-                />
-                {errors.password && (
-                  <p className="text-red-600" role="alert">
-                    {errors.password.message}
-                  </p>
-                )}
-              </div>
+      <div className="border border-[#c6c6c6] h-dvh place-content-center flex flex-col gap-6 p-6 bg-white">
+        <h1 className="text-2xl py-3 font-semibold block lg:hidden md:hidden sm:block xs:block">
+          Welcome to jobtracker
+        </h1>
+        {!userHasAccount ? (
+          <form className="flex flex-col gap-4 text-sm" onSubmit={handleSignup}>
+            <div className="flex flex-col gap-1">
+              <label>Name</label>
               <input
-                className="bg-black text-white min-w-fit py-4 px-1 rounded-full font-medium cursor-pointer select-none"
-                type="submit"
-                value="Continue"
+                type="text"
+                className={clsx(
+                  errors.name && "border-red-600",
+                  "border border-[#c6c6c6] rounded-md px-4 py-2 focus:outline-none"
+                )}
+                placeholder="Name"
+                {...register("name", {
+                  pattern: {
+                    value: /^[A-Za-z]+$/i,
+                    message: "Name cannot have symbols or special characters",
+                  },
+                  required: {
+                    value: true,
+                    message: "Name is required",
+                  },
+                })}
               />
-              <div>
-                Already have an account?{" "}
-                <button
-                  className="underline text-[#0000EE]"
-                  onClick={handleClick}
-                >
-                  Sign in
-                </button>
-              </div>
-            </form>
-          ) : (
-            <form
-              className="flex flex-col gap-4 text-sm"
-              onSubmit={handleLogin}
-            >
-              <div className="flex flex-col gap-1">
-                <label>Email</label>
-                <input
-                  type="text"
-                  className={clsx(
-                    errors.email && "border-red-600",
-                    "border border-[#c6c6c6] rounded-md px-4 py-2 focus:outline-none"
-                  )}
-                  placeholder="Email"
-                  {...register("email", {
-                    required: {
-                      value: true,
-                      message: "Email is required",
-                    },
-                    pattern: {
-                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                      message: "Email is invalid",
-                    },
-                  })}
-                />
-                {errors.email && (
-                  <p className="text-red-600" role="alert">
-                    {errors.email.message}
-                  </p>
-                )}
-              </div>
-              <div className="flex flex-col gap-1">
-                <label>Password</label>
-                <input
-                  type="password"
-                  className={clsx(
-                    errors.password && "border-red-600",
-                    "border border-[#c6c6c6] rounded-md px-4 py-2 focus:outline-none"
-                  )}
-                  placeholder="Password"
-                  {...register("password", {
-                    required: {
-                      value: true,
-                      message: "Password is required",
-                    },
-                  })}
-                />
-                {errors.password && (
-                  <p className="text-red-600" role="alert">
-                    {errors.password.message}
-                  </p>
-                )}
-              </div>
+              {errors.name && (
+                <p className="text-red-600 top-0 relative" role="alert">
+                  {errors.name.message}
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col gap-1">
+              <label>Email</label>
               <input
-                className="bg-black text-white min-w-fit py-4 px-1 rounded-full font-medium cursor-pointer select-none"
-                type="submit"
-                value="Continue"
+                type="text"
+                className={clsx(
+                  errors.email && "border-red-600",
+                  "border border-[#c6c6c6] rounded-md px-4 py-2 focus:outline-none"
+                )}
+                placeholder="Email"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is required",
+                  },
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: "Email is invalid",
+                  },
+                })}
               />
-              <div>
-                Don't have an account?{" "}
-                <button
-                  className="underline text-[#0000EE]"
-                  onClick={handleClick}
-                >
-                  Sign up
-                </button>
-              </div>
-            </form>
-          )}
-        </div>
+              {errors.email && (
+                <p className="text-red-600" role="alert">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col gap-1">
+              <label>Password</label>
+              <input
+                type="password"
+                className={clsx(
+                  errors.password && "border-red-600",
+                  "border border-[#c6c6c6] rounded-md px-4 py-2 focus:outline-none"
+                )}
+                placeholder="Password (6 or more characters)"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password is required",
+                  },
+                  minLength: {
+                    value: 6,
+                    message: "Password must be 6 characters or more",
+                  },
+                })}
+              />
+              {errors.password && (
+                <p className="text-red-600" role="alert">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+            <input
+              className="bg-black text-white min-w-fit py-4 px-1 rounded-full font-medium cursor-pointer select-none"
+              type="submit"
+              value="Continue"
+            />
+            <div>
+              Already have an account?{" "}
+              <button
+                className="underline text-[#0000EE]"
+                onClick={handleClick}
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        ) : (
+          <form className="flex flex-col gap-4 text-sm" onSubmit={handleLogin}>
+            <div className="flex flex-col gap-1">
+              <label>Email</label>
+              <input
+                type="text"
+                className={clsx(
+                  errors.email && "border-red-600",
+                  "border border-[#c6c6c6] rounded-md px-4 py-2 focus:outline-none"
+                )}
+                placeholder="Email"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is required",
+                  },
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: "Email is invalid",
+                  },
+                })}
+              />
+              {errors.email && (
+                <p className="text-red-600" role="alert">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col gap-1">
+              <label>Password</label>
+              <input
+                type="password"
+                className={clsx(
+                  errors.password && "border-red-600",
+                  "border border-[#c6c6c6] rounded-md px-4 py-2 focus:outline-none"
+                )}
+                placeholder="Password"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password is required",
+                  },
+                })}
+              />
+              {errors.password && (
+                <p className="text-red-600" role="alert">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+            <input
+              className="bg-black text-white min-w-fit py-4 px-1 rounded-full font-medium cursor-pointer select-none"
+              type="submit"
+              value="Continue"
+            />
+            <div>
+              Don't have an account?{" "}
+              <button
+                className="underline text-[#0000EE]"
+                onClick={handleClick}
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+        )}
       </div>
-      <div className="place-content-center text-center hidden lg:block md:block sm:hidden xs:hidden">
+      <div className="place-content-center text-center hidden lg:block md:block sm:hidden xs:hidden p-6">
         <h1 className="font-semibold text-3xl lg:text-3xl md:text-2xl py-3">
           Welcome to jobtracker
         </h1>
