b9227d84933bf7be11ad7ef8811f99cf2f73648f
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2025-03-19 22:41:22 +1300

    User can change their display name

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,19 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { Modal, Tooltip } from "@mui/material";
+import { updateProfile } from "firebase/auth";
+import InfoIcon from "@mui/icons-material/Info";
 
 export default function Profile() {
   const { user } = useContext(AuthContext);
+  const [name, setName] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
 
   if (!user) return null;
+
+  const handleSubmit = () => {
+    updateProfile(user, { displayName: name });
+  };
 
   return (
     <div className="py-12 px-4 lg:px-40 md:px-22 sm:px-16 xs:px-4 flex flex-col gap-2">
@@ -13,7 +22,30 @@ export default function Profile() {
       </h1>
       <div className="text-sm [&>div]:max-w-64 [&>div]:mb-3">
         <div>
-          <label>Name</label>
+          <div className="flex justify-between">
+            <div>
+              <label className="mr-1">Name</label>
+              <Tooltip
+                placement="top"
+                title="A name change may require a page refresh"
+                followCursor
+                enterTouchDelay={0}
+              >
+                <InfoIcon fontSize="small" />
+              </Tooltip>
+            </div>
+            {!isOpen && (
+              <button
+                onClick={() => {
+                  setName(user.displayName as string);
+                  setIsOpen((isOpen) => !isOpen);
+                }}
+                className="text-[#0000EE]"
+              >
+                Edit
+              </button>
+            )}
+          </div>
           <input
             type="text"
             className="w-full rounded-md border border-[#c6c6c6] px-4 py-2 mt-1"
@@ -40,6 +72,37 @@ export default function Profile() {
           />
         </div>
       </div>
+      {isOpen && (
+        <Modal keepMounted open={isOpen}>
+          <form
+            onSubmit={handleSubmit}
+            className="text-sm [&>div]:max-w-64 h-full grid place-content-center"
+          >
+            <div>
+              <label>Name</label>
+              <input
+                type="text"
+                className="w-full rounded-md border border-[#c6c6c6] px-4 py-2 mt-1 text-black"
+                value={name || ""}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="w-full flex justify-between mt-2">
+              <button
+                onClick={() => setIsOpen((isOpen) => !isOpen)}
+                className="w-full cursor-pointer font-medium dark:bg-inherit dark:text-white bg-white text-black underline hover:no-underline px-4 py-2 rounded-full"
+              >
+                Cancel
+              </button>
+              <input
+                type="submit"
+                value="Confirm"
+                className="w-full cursor-pointer font-medium dark:bg-white dark:text-gray-night bg-black text-white rounded-full px-4 py-2"
+              />
+            </div>
+          </form>
+        </Modal>
+      )}
     </div>
   );
 }
