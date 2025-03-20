c48b7a4469bcb26a8f59c1665218799858bf2555
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2025-03-20 15:52:10 +1300

    Update name value with RHF

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,17 +3,31 @@ import { AuthContext } from "../context/AuthContext";
 import { Modal, Tooltip } from "@mui/material";
 import { updateProfile } from "firebase/auth";
 import InfoIcon from "@mui/icons-material/Info";
+import { useForm } from "react-hook-form";
+import clsx from "clsx";
 
 export default function Profile() {
   const { user } = useContext(AuthContext);
-  const [name, setName] = useState("");
   const [isOpen, setIsOpen] = useState(false);
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: user?.displayName,
+    },
+  });
 
   if (!user) return null;
 
-  const handleSubmit = () => {
-    updateProfile(user, { displayName: name });
-  };
+  const onSubmit = handleSubmit(async (data) =>
+    updateProfile(user, { displayName: data.name }).then(() => {
+      window.location.reload();
+    })
+  );
 
   return (
     <div className="py-12 px-4 lg:px-40 md:px-22 sm:px-16 xs:px-4 flex flex-col gap-2">
@@ -27,7 +41,7 @@ export default function Profile() {
             {!isOpen && (
               <button
                 onClick={() => {
-                  setName(user.displayName as string);
+                  setValue("name", user.displayName as string);
                   setIsOpen((isOpen) => !isOpen);
                 }}
                 className="text-sky-600 underline hover:no-underline"
@@ -65,8 +79,8 @@ export default function Profile() {
       {isOpen && (
         <Modal keepMounted open={isOpen}>
           <form
-            onSubmit={handleSubmit}
-            className="text-sm [&>div]:max-w-64 h-full grid place-content-center"
+            onSubmit={onSubmit}
+            className="text-sm [&>div]:max-w-64 h-full grid place-content-center max-w-min m-auto"
           >
             <div className="flex justify-between">
               <label className="mr-1 text-white">Name</label>
@@ -84,13 +98,35 @@ export default function Profile() {
             </div>
             <input
               type="text"
-              className="w-full rounded-md border border-[#c6c6c6] px-4 py-2 mt-1 text-black"
-              value={name || ""}
-              onChange={(e) => setName(e.target.value)}
+              className={clsx(
+                errors.name && "border-red-600",
+                "border border-[#c6c6c6] rounded-md px-4 py-2 mt-1 focus:outline-none"
+              )}
+              {...register("name", {
+                pattern: {
+                  value: /^[A-Za-z]+$/i,
+                  message: "Name cannot have symbols or special characters",
+                },
+                required: {
+                  value: true,
+                  message: "Name is required",
+                },
+              })}
+              onChange={(e) =>
+                setValue("name", e.target.value, { shouldValidate: true })
+              }
             />
+            {errors.name && (
+              <p className="text-red-600" role="alert">
+                {errors.name.message}
+              </p>
+            )}
             <div className="w-full flex justify-between mt-2">
               <button
-                onClick={() => setIsOpen((isOpen) => !isOpen)}
+                onClick={() => {
+                  setIsOpen((isOpen) => !isOpen);
+                  reset();
+                }}
                 className="w-full cursor-pointer font-medium bg-transparent text-white underline hover:no-underline px-4 py-2 rounded-full"
               >
                 Cancel
