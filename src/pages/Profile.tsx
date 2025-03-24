ae0562833e8f803701f19e01c033d4ac5065b3b0
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2025-03-24 18:54:09 +1300

    Apply background color to wrapper

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -80,62 +80,64 @@ export default function Profile() {
         <Modal keepMounted open={isOpen}>
           <form
             onSubmit={onSubmit}
-            className="text-sm [&>div]:max-w-64 h-full grid place-content-center max-w-min m-auto"
+            className="[&>div]:max-w-64 h-full self-center items-center relative grid place-content-center max-w-min m-auto"
           >
-            <div className="flex justify-between">
-              <label className="mr-1 text-white">Name</label>
-              <Tooltip
-                placement="top"
-                title="A name change may require a page refresh"
-                followCursor
-                enterTouchDelay={0}
-              >
-                <InfoIcon
-                  fontSize="small"
-                  className="!fill-[#fff] dark:fill-[#000]"
-                />
-              </Tooltip>
-            </div>
-            <input
-              type="text"
-              className={clsx(
-                errors.name && "border-red-600",
-                "border border-[#c6c6c6] rounded-md px-4 py-2 mt-1 text-black focus:outline-none"
-              )}
-              {...register("name", {
-                pattern: {
-                  value: /^[A-Za-z]+$/i,
-                  message: "Name cannot have symbols or special characters",
-                },
-                required: {
-                  value: true,
-                  message: "Name is required",
-                },
-              })}
-              onChange={(e) =>
-                setValue("name", e.target.value, { shouldValidate: true })
-              }
-            />
-            {errors.name && (
-              <p className="text-red-600" role="alert">
-                {errors.name.message}
-              </p>
-            )}
-            <div className="w-full flex justify-between mt-2">
-              <button
-                onClick={() => {
-                  setIsOpen((isOpen) => !isOpen);
-                  reset();
-                }}
-                className="w-full cursor-pointer font-medium bg-transparent text-white underline hover:no-underline px-4 py-2 rounded-full"
-              >
-                Cancel
-              </button>
+            <div className="bg-white dark:bg-[#18181B] text-sm p-6 rounded-md shadow-lg">
+              <div className="flex justify-between">
+                <label className="mr-1 text-black dark:text-white">Name</label>
+                <Tooltip
+                  placement="top"
+                  title="A name change may require a page refresh"
+                  followCursor
+                  enterTouchDelay={0}
+                >
+                  <InfoIcon
+                    fontSize="small"
+                    className="!fill-[#fff] dark:fill-[#000]"
+                  />
+                </Tooltip>
+              </div>
               <input
-                type="submit"
-                value="Confirm"
-                className="w-full cursor-pointer font-medium dark:bg-white dark:text-gray-night bg-black text-white rounded-full px-4 py-2"
+                type="text"
+                className={clsx(
+                  errors.name && "border-red-600",
+                  "border border-[#c6c6c6] rounded-md px-4 py-2 mt-1 text-black focus:outline-none"
+                )}
+                {...register("name", {
+                  pattern: {
+                    value: /^[A-Za-z]+$/i,
+                    message: "Name cannot have symbols or special characters",
+                  },
+                  required: {
+                    value: true,
+                    message: "Name is required",
+                  },
+                })}
+                onChange={(e) =>
+                  setValue("name", e.target.value, { shouldValidate: true })
+                }
               />
+              {errors.name && (
+                <p className="text-red-600 mt-1" role="alert">
+                  {errors.name.message}
+                </p>
+              )}
+              <div className="w-full flex justify-between mt-2">
+                <button
+                  onClick={() => {
+                    setIsOpen((isOpen) => !isOpen);
+                    reset();
+                  }}
+                  className="w-full cursor-pointer font-medium bg-transparent text-black dark:text-white underline hover:no-underline px-4 py-2 rounded-full"
+                >
+                  Cancel
+                </button>
+                <input
+                  type="submit"
+                  value="Confirm"
+                  className="w-full cursor-pointer font-medium dark:bg-white dark:text-gray-night bg-black text-white rounded-full px-4 py-2"
+                />
+              </div>
             </div>
           </form>
         </Modal>
