291b7197aeecfee8b3d4ce27c63d4de2adfb5820
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2025-03-31 20:16:54 +1300

    create responsive drawer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,61 @@
 import { useContext, useRef } from "react";
 import { AuthContext } from "../context/AuthContext";
-import { useMouse, useTheme } from "../hooks";
+import useWindowDimensions, { useMouse, useTheme } from "../hooks";
 import { Link } from "react-router-dom";
 import Toggle, { IOSSwitch } from "./Toggle";
+import clsx from "clsx";
+import { Backdrop } from "@mui/material";
 
 export default function Header() {
   const ref = useRef<HTMLDivElement>(null);
   const { user, signOutUser } = useContext(AuthContext);
   const { clicked, handleClick } = useMouse(ref);
   const { theme, handleThemeChange } = useTheme();
+  const { width } = useWindowDimensions();
   const placeholder = user?.displayName?.[0].toUpperCase();
+
+  const DrawerList = () => (
+    <>
+      <div className="p-4">
+        <h2 className="font-medium">{user?.displayName}</h2>
+        <div>{user?.email}</div>
+      </div>
+      <div>
+        <Link
+          to="profile"
+          className="inline-block w-full mt-1 py-2 px-4 cursor-pointer dark:hover:bg-[#2b2b2b] hover:bg-[#f5f5f5]"
+        >
+          Profile
+        </Link>
+        <Link
+          to="applications"
+          className="inline-block w-full mb-1 py-2 px-4 cursor-pointer dark:hover:bg-[#2b2b2b] hover:bg-[#f5f5f5]"
+        >
+          Applications
+        </Link>
+      </div>
+      <div>
+        <div className="my-1 py-2 px-4 flex flex-row justify-between items-center">
+          <span>Dark mode</span>
+          <Toggle
+            sx={{ m: 0 }}
+            label={false}
+            onClick={handleThemeChange}
+            checked={theme === "dark"}
+            control={<IOSSwitch />}
+          />
+        </div>
+      </div>
+      <div>
+        <button
+          className="inline-block w-full text-start mt-1 mb-2 py-2 px-4 cursor-pointer dark:hover:bg-[#2b2b2b] hover:bg-[#f5f5f5]"
+          onClick={signOutUser}
+        >
+          Sign out
+        </button>
+      </div>
+    </>
+  );
 
   return (
     <div className="sticky top-0 z-10 border-b dark:border-[#ffffff18]">
@@ -22,45 +68,25 @@ export default function Header() {
           >
             {placeholder}
           </button>
-          {clicked && (
-            <div className="divide-y-[1px] dark:divide-[#ffffff18] border border-[#c6c6c6] dark:border-none absolute top-10 right-0 z-40 p-3 shadow-lg rounded-md min-w-52 w-auto dark:bg-[#18181B] bg-white text-sm">
-              <div className="pb-2 px-2">
-                <h2 className="font-medium">{user?.displayName}</h2>
-                <div>{user?.email}</div>
-              </div>
-              <div className="py-2">
-                <Link
-                  to="profile"
-                  className="inline-block w-full py-1 px-2 cursor-pointer dark:hover:bg-[#2b2b2b] hover:bg-[#f5f5f5] hover:rounded-md"
-                >
-                  Profile
-                </Link>
-                <Link
-                  to="applications"
-                  className="inline-block w-full py-1 px-2 cursor-pointer dark:hover:bg-[#2b2b2b] hover:bg-[#f5f5f5] hover:rounded-md"
-                >
-                  Applications
-                </Link>
-              </div>
-              <div className="py-2">
-                <div className="py-1 px-2 flex flex-row justify-between items-center">
-                  <span>Dark mode</span>
-                  <Toggle
-                    sx={{ m: 0 }}
-                    label={false}
-                    onClick={handleThemeChange}
-                    checked={theme === "dark"}
-                    control={<IOSSwitch />}
-                  />
-                </div>
-              </div>
-              <div className="pt-2">
-                <button
-                  className="inline-block w-full text-start py-1 px-2 cursor-pointer dark:hover:bg-[#2b2b2b] hover:bg-[#f5f5f5] hover:rounded-md"
-                  onClick={signOutUser}
-                >
-                  Sign out
-                </button>
+          {width >= 640 ? (
+            <div
+              className={clsx(
+                clicked && "!block",
+                "hidden divide-y-[1px] dark:divide-[#ffffff18] border border-[#c6c6c6] dark:border-none absolute top-10 right-0 z-40 shadow-lg rounded-md min-w-52 w-auto dark:bg-[#18181B] bg-white text-sm"
+              )}
+            >
+              <DrawerList />
+            </div>
+          ) : (
+            <div>
+              <Backdrop open={clicked} onClick={handleClick} />
+              <div
+                className={clsx(
+                  clicked && "!block",
+                  "hidden divide-y-[1px] dark:divide-[#ffffff18] border border-[#c6c6c6] dark:border-none fixed h-dvh top-0 right-0 z-40 shadow-lg min-w-64 w-auto dark:bg-[#18181B] bg-white text-sm"
+                )}
+              >
+                <DrawerList />
               </div>
             </div>
           )}
