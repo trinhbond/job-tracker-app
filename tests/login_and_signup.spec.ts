adc6a1762ce2189bd44f5a994a5557d11cf2deec
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2025-06-23 20:20:42 +1200

    fix login/signup tests

--- tests/login_and_signup.spec.ts
+++ tests/login_and_signup.spec.ts
@@ -4,14 +4,10 @@ const baseURL = "http://localhost:3000";
 
 test.beforeEach(async ({ page }) => {
   await page.goto(baseURL);
+  await page.waitForSelector("div.overflow-x-hidden", { timeout: 3000 });
 });
 
-test("has title", async ({ page }) => {
-  await expect(page).toHaveTitle(/Job Tracker/);
-});
-
-test("signup form errors", async ({ page }) => {
-  // No inputs
+test("test signup form errors", async ({ page }) => {
   await page.locator("input", { hasText: "Continue" }).click();
   await Promise.all([
     expect(page.locator("p", { hasText: "Name is required" })).toBeVisible(),
@@ -21,7 +17,6 @@ test("signup form errors", async ({ page }) => {
     ).toBeVisible(),
   ]);
 
-  // Invalid input
   await page.locator("form > div:nth-child(1) > input").fill("23");
   await page.locator("input", { hasText: "Continue" }).click();
   await expect(
@@ -33,23 +28,18 @@ test("signup form errors", async ({ page }) => {
   await page.locator("form > div:nth-child(2) > input").fill("23");
   await page.locator("input", { hasText: "Continue" }).click();
   await expect(
-    page.locator("p", {
-      hasText: "Email is invalid",
-    })
+    page.locator("p", { hasText: "Email is invalid" })
   ).toBeVisible();
 
   await page.locator("form > div:nth-child(3) > input").fill("23");
   await page.locator("input", { hasText: "Continue" }).click();
   await expect(
-    page.locator("p", {
-      hasText: "Password must be 6 characters or more",
-    })
+    page.locator("p", { hasText: "Password must be 6 characters or more" })
   ).toBeVisible();
 });
 
-test("login form errors", async ({ page }) => {
+test("test login form errors", async ({ page }) => {
   await page.locator("button", { hasText: "Sign in" }).click();
-
   await page.locator("input", { hasText: "Continue" }).click();
 
   await Promise.all([
@@ -59,12 +49,10 @@ test("login form errors", async ({ page }) => {
     ).toBeVisible(),
   ]);
 
-  // Invalid input
   await page.locator("form > div:nth-child(1) > input").fill("email@test.com");
   await page.locator("form > div:nth-child(2) > input").fill("password");
   await page.locator("input", { hasText: "Continue" }).click();
 
-  // Invalid credentials
   await expect(
     page.locator("p", {
       hasText: "Email or password is invalid",
