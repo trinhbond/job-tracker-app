4d3241b4acbacafb3b99d91103702a978ffa26fc
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2025-09-11 21:24:49 +1200

    Convert form elements to MUI components

--- src/components/forms/Application/CreateForm.tsx
+++ src/components/forms/Application/CreateForm.tsx
@@ -1,5 +1,13 @@
-import { FormControl, MenuItem, Modal, Select } from "@mui/material";
-import clsx from "clsx";
+import {
+  Box,
+  Button,
+  FormControl,
+  Input,
+  MenuItem,
+  Modal,
+  Select,
+  Typography,
+} from "@mui/material";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../../../config/firebase";
 import { AppForm } from "../../../types/form-types";
@@ -7,7 +15,8 @@ import { useContext, useRef } from "react";
 import { AuthContext } from "../../../context/AuthContext";
 import { notify, statusValues } from "../../../utils";
 import { Controller, useForm } from "react-hook-form";
-import { useTheme } from "../../../hooks";
+import { TextArea } from "../../TextArea";
+import { ModalContentWrapper } from "./ModalContentWrapper";
 
 export default function CreateForm({
   isModalOpen,
@@ -17,7 +26,6 @@ export default function CreateForm({
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
   const { user } = useContext(AuthContext);
-  const { isThemeDark } = useTheme();
   const {
     handleSubmit,
     reset,
@@ -44,176 +52,188 @@ export default function CreateForm({
     }
   });
 
+  const toggleModal = () => {
+    setIsModalOpen((isModalOpen) => !isModalOpen);
+    reset();
+  };
+
   return (
-    <Modal
-      open={isModalOpen}
-      onClose={() => setIsModalOpen((isModalOpen) => !isModalOpen)}
-    >
-      <div className="p-4 shadow-lg bg-white text-black fixed z-40 h-full w-full sm:w-96 lg:w-min-96 top-0 right-0 overflow-y-scroll text-sm">
-        <div className="text-xl font-medium">New application</div>
-        <form
-          className="flex flex-col gap-4 mt-6"
+    <Modal open={isModalOpen} onClose={toggleModal}>
+      <ModalContentWrapper width={{ xs: "100%", sm: 384, md: 384, lg: 384 }}>
+        <Box component="div" fontSize={20} fontWeight={500}>
+          New application
+        </Box>
+        <Box
+          className="create-form"
+          component="form"
+          display="flex"
+          flexDirection="column"
+          gap={2}
+          mt={3}
           onSubmit={handleAddApplication}
         >
-          <div>
-            <label>
-              Role <span className="text-red-600">*</span>
-            </label>
-            <Controller
-              control={control}
-              name={"title"}
-              render={() => (
-                <input
-                  placeholder="Role"
-                  {...register("title", {
-                    required: {
-                      value: true,
-                      message: "Title is required",
-                    },
-                    pattern: {
-                      value: /[^' ']+/,
-                      message: "Title is required",
-                    },
-                  })}
-                  className={clsx(
-                    errors.title && "border-red-600",
-                    "w-full focus:outline-none bg-white border px-4 py-2 mt-1 rounded-md"
-                  )}
-                />
+          <Box display="flex" flexDirection="column">
+            <FormControl>
+              <Box component="label">
+                Role <span className="text-red-600">*</span>
+              </Box>
+              <Controller
+                control={control}
+                name={"title"}
+                render={() => (
+                  <Input
+                    error={!!errors.title}
+                    placeholder="Role"
+                    {...register("title", {
+                      required: {
+                        value: true,
+                        message: "Title is required",
+                      },
+                      pattern: {
+                        value: /[^' ']+/,
+                        message: "Title is required",
+                      },
+                    })}
+                  />
+                )}
+              />
+              {errors.title && (
+                <Typography component="p" role="alert">
+                  {errors.title.message}
+                </Typography>
               )}
-            />
-            {errors.title && <p role="alert">Title is required</p>}
-          </div>
-          <div>
-            <label>
-              Company <span className="text-red-600">*</span>
-            </label>
-            <Controller
-              control={control}
-              name={"company"}
-              render={() => (
-                <input
-                  placeholder="Company"
-                  className={clsx(
-                    errors.company && "border-red-600",
-                    "w-full focus:outline-none bg-white border px-4 py-2 mt-1 rounded-md"
-                  )}
-                  {...register("company", {
-                    required: {
-                      value: true,
-                      message: "Company is required",
-                    },
-                    pattern: {
-                      value: /[^' ']+/,
-                      message: "Company is required",
-                    },
-                  })}
-                />
+            </FormControl>
+          </Box>
+          <Box display="flex" flexDirection="column">
+            <FormControl>
+              <Box component="label">
+                Company <span className="text-red-600">*</span>
+              </Box>
+              <Controller
+                control={control}
+                name={"company"}
+                render={() => (
+                  <Input
+                    error={!!errors.company}
+                    placeholder="Company"
+                    {...register("company", {
+                      required: {
+                        value: true,
+                        message: "Company is required",
+                      },
+                      pattern: {
+                        value: /[^' ']+/,
+                        message: "Company is required",
+                      },
+                    })}
+                  />
+                )}
+              />
+              {errors.company && (
+                <Typography component="p" role="alert">
+                  {errors.company.message}
+                </Typography>
               )}
-            />
-            {errors.company && <p role="alert">Company is required</p>}
-          </div>
-          <div>
-            <label>Location</label>
-            <Controller
-              control={control}
-              name={"location"}
-              render={() => (
-                <input
-                  placeholder="Location"
-                  {...register("location")}
-                  className="w-full focus:outline-none bg-white border px-4 py-2 mt-1 rounded-md"
-                />
-              )}
-            />
-          </div>
-          <div>
-            <label className="text-black">Status</label>
-            <Controller
-              control={control}
-              name={"status"}
-              render={() => (
-                <FormControl fullWidth>
-                  <Select
-                    className="mt-1"
-                    {...register("status")}
-                    displayEmpty
-                    defaultValue={""}
-                  >
-                    <MenuItem value="">
-                      <em>None</em>
-                    </MenuItem>
-                    {statusValues.slice(1).map((option) => (
-                      <MenuItem value={option}>
-                        {option.slice(0, 1).toUpperCase() + option.substring(1)}
+            </FormControl>
+          </Box>
+          <Box display="flex" flexDirection="column">
+            <FormControl>
+              <Box component="label">Location</Box>
+              <Controller
+                control={control}
+                name={"location"}
+                render={() => (
+                  <Input placeholder="Location" {...register("location")} />
+                )}
+              />
+            </FormControl>
+          </Box>
+          <Box display="flex" flexDirection="column">
+            <FormControl>
+              <Box component="label">Status</Box>
+              <Controller
+                control={control}
+                name={"status"}
+                render={() => (
+                  <FormControl>
+                    <Select
+                      {...register("status")}
+                      displayEmpty
+                      defaultValue={""}
+                    >
+                      <MenuItem value="">
+                        <em>None</em>
                       </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              )}
-            />
-          </div>
-          <div>
-            <label>Link</label>
-            <Controller
-              control={control}
-              name={"link"}
-              render={() => (
-                <input
-                  placeholder="Link"
-                  {...register("link")}
-                  className="w-full focus:outline-none bg-white border px-4 py-2 mt-1 rounded-md"
-                />
-              )}
-            />
-          </div>
-          <div>
-            <label>Salary</label>
-            <Controller
-              control={control}
-              name={"salary"}
-              render={() => (
-                <input
-                  type="number"
-                  placeholder="Salary"
-                  {...register("salary")}
-                  className="w-full focus:outline-none bg-white border px-4 py-2 mt-1 rounded-md"
-                />
-              )}
-            />
-          </div>
-          <div>
-            <label>Notes</label>
-            <Controller
-              control={control}
-              name={"notes"}
-              render={() => (
-                <textarea
-                  className="resize-none min-h-[150px] w-full focus:outline-none bg-white border px-4 py-2 mt-1 rounded-md"
-                  placeholder="Notes"
-                  {...register("notes")}
-                />
-              )}
-            />
-          </div>
-          <div className="text-xs">
-            <input
-              className="cursor-pointer	font-medium bg-[#f2f2f3] hover:bg-[#eaeaeb] rounded-full text-black px-4 py-2"
-              type="submit"
-              value="Confirm"
-            />
-            <input
-              className="cursor-pointer	font-medium bg-white text-black underline hover:no-underline px-4 py-2 rounded-full"
-              type="button"
-              value="Cancel"
-              onClick={() => {
-                setIsModalOpen((isModalOpen) => !isModalOpen);
-                reset();
-              }}
-            />
-          </div>
-        </form>
-      </div>
+                      {statusValues.slice(1).map((option) => (
+                        <MenuItem value={option}>
+                          {option.slice(0, 1).toUpperCase() +
+                            option.substring(1)}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                )}
+              />
+            </FormControl>
+          </Box>
+          <Box display="flex" flexDirection="column">
+            <FormControl>
+              <Box component="label">Link</Box>
+              <Controller
+                control={control}
+                name={"link"}
+                render={() => (
+                  <Input placeholder="Link" {...register("link")} />
+                )}
+              />
+            </FormControl>
+          </Box>
+          <Box display="flex" flexDirection="column">
+            <FormControl>
+              <Box component="label">Salary</Box>
+              <Controller
+                control={control}
+                name={"salary"}
+                render={() => (
+                  <Input
+                    type="number"
+                    placeholder="Salary"
+                    {...register("salary")}
+                  />
+                )}
+              />
+            </FormControl>
+          </Box>
+          <Box display="flex" flexDirection="column">
+            <FormControl>
+              <Box component="label">Notes</Box>
+              <Controller
+                control={control}
+                name={"notes"}
+                render={() => (
+                  <TextArea placeholder="Notes" {...register("notes")} />
+                )}
+              />
+            </FormControl>
+          </Box>
+          <Box>
+            <Button variant="contained" type="submit">
+              Confirm
+            </Button>
+            <Box display="inline-block" ml={1} paddingX={1} paddingY={2}>
+              <Button
+                variant="text"
+                sx={{
+                  color: "#000",
+                }}
+                onClick={toggleModal}
+              >
+                Cancel
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </ModalContentWrapper>
     </Modal>
   );
 }
--- src/components/forms/Application/EditForm.tsx
+++ src/components/forms/Application/EditForm.tsx
@@ -1,12 +1,14 @@
 import {
-  FormControl,
+  Box,
+  Button,
+  Input,
   MenuItem,
   Modal,
   Select,
   SelectChangeEvent,
+  Typography,
 } from "@mui/material";
 import { AppForm } from "../../../types/form-types";
-import clsx from "clsx";
 import { useForm } from "react-hook-form";
 import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { db } from "../../../config/firebase";
@@ -14,7 +16,8 @@ import { handleChange, notify, statusValues } from "../../../utils";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useContext, useRef } from "react";
 import { AuthContext } from "../../../context/AuthContext";
-import { useTheme } from "../../../hooks";
+import { TextArea } from "../../TextArea";
+import { ModalContentWrapper } from "./ModalContentWrapper";
 
 export default function EditForm({
   data,
@@ -29,7 +32,6 @@ export default function EditForm({
   setShowSelectedData: React.Dispatch<React.SetStateAction<any>>;
   showSelectedData: any;
 }) {
-  const { isThemeDark } = useTheme();
   const { user } = useContext(AuthContext);
   const { reset } = useForm<AppForm>();
   const toastId = useRef("toast");
@@ -57,175 +59,180 @@ export default function EditForm({
 
   const handleDelete = (id: string) => {
     deleteDoc(doc(db, "applications", "user/", user?.uid as string, id));
+    notify("Application deleted", "success", toastId);
   };
 
+  const handleToggle = () => {
+    setShowSelectedData({});
+    reset();
+  };
   return (
     <>
       {data.map((props) => (
-        <Modal
-          open={showSelectedData[props.id]}
-          onClose={() => setShowSelectedData({})}
-        >
-          <div className="p-4 shadow-lg text-black bg-white fixed z-40 h-full w-full sm:w-96 lg:w-min-96 top-0 right-0 overflow-y-scroll text-sm">
-            <div>
-              <div className="text-xl font-medium">Edit application</div>
-              <form
-                className="flex flex-col gap-4 mt-6"
-                onSubmit={(event) => handleEditApplication(props.id, event)}
+        <Modal open={showSelectedData[props.id]} onClose={handleToggle}>
+          <ModalContentWrapper
+            width={{ xs: "100%", sm: 384, md: 384, lg: 384 }}
+          >
+            <Box component="div" fontSize={20} fontWeight={500}>
+              Edit application
+            </Box>
+            <Box
+              className="edit-form"
+              component="form"
+              display="flex"
+              flexDirection="column"
+              gap={2}
+              mt={3}
+              onSubmit={(event) => handleEditApplication(props.id, event)}
+            >
+              <Box display="flex" flexDirection="column">
+                <Box component="label">
+                  Role <span className="text-red-600">*</span>
+                </Box>
+                <Input
+                  error={!prevData.title.trim()}
+                  placeholder="Role"
+                  name="title"
+                  id="title"
+                  value={prevData.title}
+                  onChange={(event) =>
+                    handleChange(event, prevData, setPrevData)
+                  }
+                />
+                {!prevData.title.trim() && (
+                  <Typography component="p" role="alert">
+                    Title is required
+                  </Typography>
+                )}
+              </Box>
+              <Box display="flex" flexDirection="column">
+                <Box component="label">
+                  Company
+                  <span className="text-red-600">*</span>
+                </Box>
+                <Input
+                  error={!prevData.company.trim()}
+                  placeholder="Company"
+                  name="company"
+                  id="company"
+                  value={prevData.company}
+                  onChange={(event) =>
+                    handleChange(event, prevData, setPrevData)
+                  }
+                />
+                {!prevData.company.trim() && (
+                  <Typography component="p" role="alert">
+                    Company is required
+                  </Typography>
+                )}
+              </Box>
+              <Box display="flex" flexDirection="column">
+                <Box component="label">Location</Box>
+                <Input
+                  placeholder="Location"
+                  name="location"
+                  id="location"
+                  value={prevData.location}
+                  onChange={(event) =>
+                    handleChange(event, prevData, setPrevData)
+                  }
+                />
+              </Box>
+              <Box display="flex" flexDirection="column">
+                <Box component="label">Status</Box>
+                <Select
+                  name="status"
+                  id="status"
+                  value={prevData.status}
+                  onChange={(event: SelectChangeEvent) => {
+                    const { name, value } = event.target;
+                    setPrevData({ ...prevData, [name]: value });
+                  }}
+                  displayEmpty
+                  defaultValue={""}
+                >
+                  <MenuItem value={""}>
+                    <em>None</em>
+                  </MenuItem>
+                  {statusValues.slice(1).map((option) => (
+                    <MenuItem value={option}>
+                      {option.slice(0, 1).toUpperCase() + option.substring(1)}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </Box>
+              <Box display="flex" flexDirection="column">
+                <Box component="label">Link</Box>
+                <Input
+                  placeholder="Link"
+                  name="link"
+                  id="link"
+                  value={prevData.link}
+                  onChange={(event) =>
+                    handleChange(event, prevData, setPrevData)
+                  }
+                />
+              </Box>
+              <Box display="flex" flexDirection="column">
+                <Box component="label">Salary</Box>
+                <Input
+                  type="number"
+                  placeholder="Salary"
+                  name="salary"
+                  id="salary"
+                  value={prevData.salary}
+                  onChange={(event) =>
+                    handleChange(event, prevData, setPrevData)
+                  }
+                />
+              </Box>
+              <Box display="flex" flexDirection="column">
+                <Box component="label">Notes</Box>
+                <TextArea
+                  placeholder="Notes"
+                  name="notes"
+                  id="notes"
+                  value={prevData.notes}
+                  onChange={(event) =>
+                    handleChange(event, prevData, setPrevData)
+                  }
+                />
+              </Box>
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                flexWrap="wrap"
+                alignItems="center"
+                gap={1}
               >
-                <div>
-                  <label>
-                    Role <span className="text-red-600">*</span>
-                  </label>
-                  <input
-                    placeholder="Role"
-                    name="title"
-                    id="title"
-                    value={prevData.title}
-                    onChange={(event) =>
-                      handleChange(event, prevData, setPrevData)
-                    }
-                    className={clsx(
-                      !prevData.title.trim() && "border-red-600",
-                      "w-full focus:outline-none bg-white border px-4 py-2 mt-1 rounded-md"
-                    )}
-                  />
-                  {!prevData.title.trim() && (
-                    <p role="alert">Title is required</p>
-                  )}
-                </div>
-                <div>
-                  <label>
-                    Company <span className="text-red-600">*</span>
-                  </label>
-                  <input
-                    placeholder="Company"
-                    name="company"
-                    id="company"
-                    value={prevData.company}
-                    onChange={(event) =>
-                      handleChange(event, prevData, setPrevData)
-                    }
-                    className={clsx(
-                      !prevData.company.trim() && "border-red-600",
-                      "w-full focus:outline-none bg-white border px-4 py-2 mt-1 rounded-md"
-                    )}
-                  />
-                  {!prevData.company.trim() && (
-                    <p role="alert">Company is required</p>
-                  )}
-                </div>
-                <div>
-                  <label>Location</label>
-                  <input
-                    placeholder="Location"
-                    name="location"
-                    id="location"
-                    value={prevData.location}
-                    onChange={(event) =>
-                      handleChange(event, prevData, setPrevData)
-                    }
-                    className="w-full focus:outline-none bg-white border px-4 py-2 mt-1 rounded-md"
-                  />
-                </div>
-                <div>
-                  <label>Status</label>
-                  <FormControl fullWidth>
-                    <Select
-                      className="mt-1"
-                      name="status"
-                      id="status"
-                      value={prevData.status}
-                      onChange={(event: SelectChangeEvent) => {
-                        const { name, value } = event.target;
-                        setPrevData({ ...prevData, [name]: value });
+                <Box>
+                  <Button type="submit" variant="contained">
+                    Confirm
+                  </Button>
+                  <Box display="inline-block" ml={1} paddingX={1} paddingY={2}>
+                    <Button
+                      variant="text"
+                      sx={{
+                        color: "#000",
                       }}
-                      displayEmpty
-                      defaultValue={""}
+                      onClick={handleToggle}
                     >
-                      <MenuItem value={""}>
-                        <em>None</em>
-                      </MenuItem>
-                      {statusValues.slice(1).map((option) => (
-                        <MenuItem value={option}>
-                          {option.slice(0, 1).toUpperCase() +
-                            option.substring(1)}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </div>
-                <div>
-                  <label>Link</label>
-                  <input
-                    placeholder="Link"
-                    name="link"
-                    id="link"
-                    value={prevData.link}
-                    onChange={(event) =>
-                      handleChange(event, prevData, setPrevData)
-                    }
-                    className="w-full focus:outline-none bg-white border px-4 py-2 mt-1 rounded-md"
-                  />
-                </div>
-                <div>
-                  <label>Salary</label>
-                  <input
-                    className="w-full focus:outline-none bg-white border px-4 py-2 mt-1 rounded-md"
-                    type="number"
-                    placeholder="Salary"
-                    name="salary"
-                    id="salary"
-                    value={prevData.salary}
-                    onChange={(event) =>
-                      handleChange(event, prevData, setPrevData)
-                    }
-                  />
-                </div>
-                <div>
-                  <label>Notes</label>
-                  <textarea
-                    className="resize-none min-h-[150px] w-full focus:outline-none bg-white border px-4 py-2 mt-1 rounded-md"
-                    placeholder="Notes"
-                    name="notes"
-                    id="notes"
-                    value={prevData.notes}
-                    onChange={(event) =>
-                      handleChange(event, prevData, setPrevData)
-                    }
-                  />
-                </div>
-                <div className="w-full flex justify-between flex-wrap gap-2 items-center text-xs">
-                  <div>
-                    <input
-                      className="cursor-pointer font-medium bg-[#f2f2f3] hover:bg-[#eaeaeb] rounded-full text-black px-4 py-2"
-                      type="submit"
-                      value="Confirm"
-                      id="confirm"
-                    />
-                    <input
-                      className="cursor-pointer	font-medium text-black bg-white underline hover:no-underline px-4 py-2 rounded-full"
-                      type="button"
-                      value="Cancel"
-                      onClick={() => {
-                        setShowSelectedData({});
-                        reset();
-                      }}
-                    />
-                  </div>
-                  <div className="float-right">
-                    <button
-                      onClick={() => handleDelete(prevData.id)}
-                      className="flex items-center gap-1.5 font-medium bg-[#f2f2f3] hover:bg-[#eaeaeb] rounded-full text-black pl-3 pr-4 py-2"
-                    >
-                      <DeleteIcon className="shrink-0" fontSize="small" />
-                      <span>Delete</span>
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
+                      Cancel
+                    </Button>
+                  </Box>
+                </Box>
+                <Button
+                  variant="contained"
+                  onClick={() => handleDelete(prevData.id)}
+                >
+                  <DeleteIcon fontSize="small" />
+                  <Box component="span" pl={0.5}>
+                    Delete
+                  </Box>
+                </Button>
+              </Box>
+            </Box>
+          </ModalContentWrapper>
         </Modal>
       ))}
     </>
