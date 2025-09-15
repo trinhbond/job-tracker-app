4a7cc7daa07405a692331ca65e58cc61d360f492
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2025-09-15 20:59:51 +1200

    rename folder paths

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as motion from "motion/react-client";
 import { Box, Typography } from "@mui/material";
-import { LoginForm, SignupForm } from "../components/forms/Auth";
+import { LoginForm, SignupForm } from "../components/forms/auth";
 
 type User = {
   name: string;
